9e19bdcfc49f7d70fdd39cc288c8b2815da66f61
齋藤勇太 <>
2021-05-24 21:58:46 +0900

    ・本の検索画面のレイアウト作成 ・isbnで図書館に本があるかを検索する画面レイアウト作成

---
+++ BookResearchApp/BookResearchApp/SearchBookCardCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchBookCardCell.swift
+//  BookResearchApp
+//
+//  Created by 齋藤勇太 on 2021/05/22.
+//
+
+import UIKit
+
+class SearchBookCardCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
