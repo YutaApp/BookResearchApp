bb587f355b51dd5588311cf8ccfbb2042f135633
齋藤勇太 <>
2021-07-01 14:13:21 +0900

    デバイスごとにStoryboardを作成してレイアウトを整える

--- BookResearchApp/BookResearchApp/SceneDelegate.swift
+++ BookResearchApp/BookResearchApp/SceneDelegate.swift
@@ -16,9 +16,61 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        let deviceTypeStoryboard = self.deviceTypeStoryboard()
+        
+        if let window = window
+        {
+            window.rootViewController = deviceTypeStoryboard.instantiateInitialViewController() as UIViewController?
+        }
+        
+        self.window?.makeKeyAndVisible()
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
+    func deviceTypeStoryboard() -> UIStoryboard
+    {
+        var storyboard = UIStoryboard()
+        let height = UIScreen.main.bounds.size.height
+        
+        if height == 736 //iPhone7Plus,8Plus
+        {
+            storyboard = UIStoryboard(name: "Main", bundle: nil)
+        }
+        else if height == 568 //iPhoneSE1
+        {
+            storyboard = UIStoryboard(name: "iPhoneSE1", bundle: nil)
+        }
+        else if height == 667 //iPhone7,8,SE2
+        {
+            storyboard = UIStoryboard(name: "iPhoneSE2", bundle: nil)
+        }
+        else if height == 812 //iPhoneX,XS,11Pro,12mini
+        {
+            storyboard = UIStoryboard(name: "iPhoneX", bundle: nil)
+        }
+        else if height == 844 //iPhone12,12Pro
+        {
+            storyboard = UIStoryboard(name: "iPhone12", bundle: nil)
+        }
+        else if height == 896 //iPhoneXS Max,XR,11,11Pro Max
+        {
+            storyboard = UIStoryboard(name: "iPhone11", bundle: nil)
+        }
+        else if height == 926 //iPhone12Pro Max
+        {
+            storyboard = UIStoryboard(name: "iPhone12ProMax", bundle: nil)
+        }
+        else
+        {
+            storyboard = UIStoryboard(name: "Main", bundle: nil)
+        }
+        
+        return storyboard
+        
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
